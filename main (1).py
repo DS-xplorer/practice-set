2abe14e6cc62e16b46376c0447bcbf9f0ffc2306
DS-xplorer <139098259+DS-xplorer@users.noreply.github.com>
2023-08-08 11:53:53 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,58 @@
+a='vibha'
+b=0.2
+c=[1,'vibha',0.33,456]
+d=('vibha',34,8.880)
+
+
+
+#(/) this operater is used to divide the number and this operater give us the quotient
+#lets understans this by an example
+8/2
+
+#(%) this operater is used to get the reminder.
+# lets under stand this by using an example.
+8%2
+
+#(**)this operter is usedto get the power of number
+2**3
+
+
+var=['kunal',0.99,'data','science',34,67,('v',78,8.99),23]
+for i in var:
+    print(i)
+    print(type(i))
+else:
+    print("loop end")
+    
+    
+    
+a=34
+b=2
+i=0
+while a%b==0:
+    i=a/b
+prinr(i)
+
+
+var=(23,18,21,45,62,93,39,55,76,39,12,43,23,73,9,6,,45,44,27,68,66)
+for i in var:
+    if i%3==0:
+        print(i)
+    else:
+        continue
+    
+    
+    
+# mutable data type are those from which we an change the the data prest in that data type. like we have an example of list ,
+var=['kunal',0.99,'data','science',34,67,23]
+var[0]='vishu'
+so we can easily change it .
+
+
+# immutable data type are those type from which we cannot change the element present in it.like we have an example of string.
+a='12345678"
+a[3]=9
+we can not chanege it because it is immutable.
+
+
+
